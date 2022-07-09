1d5d74df76215915deae907ac0b56b2d08e1486e
Trinh <trinh.pham2277@gmail.com>
2022-07-09 23:05:15 +1000

    Adding test for spawner

--- src/test/java/enemy/zombieTest.java
+++ src/test/java/enemy/zombieTest.java
@@ -41,14 +41,83 @@ public class zombieTest {
         assertEquals(expectedPlayer, actualPlayer);
 
         // check for zombie after tick
-        int zombieCount = countEntityOfType(initDungonRes, "zombieToast");
+        int zombieCount = countEntityOfType(actualDungonRes, "zombie");
         assertEquals(1, zombieCount);
 
     }  
     
     @Test
-    @DisplayName("Test zombie is moves")
-    public void testZombieMoves() {
+    @DisplayName("Test zombiespawner is created")
+    public void testZombieSpawer() {
         DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse initDungonRes = dmc.newGame("d_zombieTest_simpleSpawn", "c_zombieTest_simpleSpawn");
+        assertEquals(1, getEntities(initDungonRes, "zombie_toast_spawner").size());
     } 
+
+    @Test
+    @DisplayName("Test zombiespawner spawns no zombie")
+    public void testZombieSpawerNone() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse initDungonRes = dmc.newGame("d_zombieTest_simpleSpawn", "c_spiderTest_basicMovement");
+        EntityResponse initPlayer = getPlayer(initDungonRes).get();
+
+        // create the expected result
+        EntityResponse expectedPlayer = new EntityResponse(initPlayer.getId(), initPlayer.getType(), new Position(1, 2), false);
+
+        // move player downward
+        DungeonResponse actualDungonRes = dmc.tick(Direction.DOWN);
+        EntityResponse actualPlayer = getPlayer(actualDungonRes).get();
+        assertEquals(expectedPlayer, actualPlayer);
+
+        // check for zombie after tick
+        int zombieCount = countEntityOfType(actualDungonRes, "zombie");
+        assertEquals(0, zombieCount);
+
+    } 
+
+    @Test
+    @DisplayName("Test zombiespawner after 10 ticks")
+    public void testZombieSpawerAfter10() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse initDungonRes = dmc.newGame("d_zombieTest_simpleSpawn", "c_zombieTest_simpleSpawn");
+
+        // create the expected result
+        initDungonRes = dmc.tick(Direction.RIGHT);
+        // check for zombie after tick
+        int zombieCount = countEntityOfType(initDungonRes, "zombie");
+        assertEquals(0, zombieCount);
+
+        for (int i = 0; i < 9; ++i) {
+            initDungonRes = dmc.tick(Direction.LEFT);
+            zombieCount = countEntityOfType(initDungonRes, "zombie");
+            assertEquals(0, zombieCount);
+    
+        }
+        initDungonRes = dmc.tick(Direction.RIGHT);
+        zombieCount = countEntityOfType(initDungonRes, "zombie");
+        assertEquals(1, zombieCount);
+
+
+    } 
+
+    @Test
+    @DisplayName("Test zombiespawner spawn multiple zombies")
+    public void testZombieSpawerMultiple() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse initDungonRes = dmc.newGame("d_zombieTest_simpleSpawn", "c_zombieTest_simpleSpawn");
+        
+        // move player downward
+        initDungonRes = dmc.tick(Direction.DOWN);
+        int zombieCount = countEntityOfType(initDungonRes, "zombieToast");
+        assertEquals(1, zombieCount);
+
+        initDungonRes = dmc.tick(Direction.DOWN);
+        zombieCount = countEntityOfType(initDungonRes, "zombieToast");
+        assertEquals(2, zombieCount);
+
+        initDungonRes = dmc.tick(Direction.DOWN);
+        zombieCount = countEntityOfType(initDungonRes, "zombieToast");
+        assertEquals(3, zombieCount);
+    } 
+
 }
