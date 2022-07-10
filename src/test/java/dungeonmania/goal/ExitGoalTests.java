e24eb659fe2453985b780b61f3f6390d5a3d03d5
Trinh <trinh.pham2277@gmail.com>
2022-07-10 14:08:50 +1000

    Simple exit test

---
+++ src/test/java/dungeonmania/goal/ExitGoalTests.java
@@ -0,0 +1,48 @@
+package dungeonmania.goal;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static dungeonmania.TestUtils.getPlayer;
+import static dungeonmania.TestUtils.getEntities;
+import static dungeonmania.TestUtils.getInventory;
+import static dungeonmania.TestUtils.getGoals;
+import static dungeonmania.TestUtils.countEntityOfType;
+import static dungeonmania.TestUtils.getValueFromConfigFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import dungeonmania.DungeonManiaController;
+import dungeonmania.response.models.BattleResponse;
+import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.response.models.EntityResponse;
+import dungeonmania.response.models.RoundResponse;
+import dungeonmania.util.Direction;
+import dungeonmania.util.Position;
+
+public class ExitGoalTests {
+
+    @Test
+    @DisplayName("Exit complete")
+    public void testMovementDown() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse initDungonRes = dmc.newGame("d_movementTest_testMovementDown", "c_movementTest_testMovementDown");
+        assertTrue(getGoals(initDungonRes).contains(":exit"));
+        assertFalse(getGoals(initDungonRes).contains(":treasure"));
+        assertFalse(getGoals(initDungonRes).contains(":boulders"));
+        assertFalse(getGoals(initDungonRes).contains(":enemies"));
+        dmc.tick(Direction.RIGHT);
+        dmc.tick(Direction.RIGHT);
+        dmc.tick(Direction.DOWN);
+        initDungonRes = dmc.tick(Direction.DOWN);
+        assertEquals("", getGoals(initDungonRes));
+    }
+    
+}
