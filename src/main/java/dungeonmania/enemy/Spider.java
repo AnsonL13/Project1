2d6f49dc2931e4b7d962f35d084b1dbdc5d41fe4
Trinh <trinh.pham2277@gmail.com>
2022-07-11 10:34:44 +1000

    fix errors to run test

--- src/main/java/dungeonmania/enemy/Spider.java
+++ src/main/java/dungeonmania/enemy/Spider.java
@@ -1,7 +1,10 @@
 package dungeonmania.enemy;
 
+import dungeonmania.util.Position;
+
 public class Spider extends Enemy {
     public Spider (int health, int attack) {
+        super(1, 1, new Position(1, 1));
     }
 
     public void move(boolean isInvicible, boolean isInvisible, int x, int y) {
