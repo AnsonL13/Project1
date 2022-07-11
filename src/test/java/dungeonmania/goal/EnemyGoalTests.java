c88ab8a97dbbabfd753e327667e0a11b81abdd94
Trinh <trinh.pham2277@gmail.com>
2022-07-11 17:31:06 +1000

    Test for enemy class goal

---
+++ src/test/java/dungeonmania/goal/EnemyGoalTests.java
@@ -0,0 +1,112 @@
+package dungeonmania.goal;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static dungeonmania.TestUtils.getPlayer;
+import static dungeonmania.TestUtils.getEntities;
+import static dungeonmania.TestUtils.getInventory;
+import static dungeonmania.TestUtils.getGoals;
+import static dungeonmania.TestUtils.countEntityOfType;
+import static dungeonmania.TestUtils.getValueFromConfigFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import dungeonmania.DungeonManiaController;
+import dungeonmania.response.models.BattleResponse;
+import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.response.models.EntityResponse;
+import dungeonmania.response.models.RoundResponse;
+import dungeonmania.util.Direction;
+import dungeonmania.util.Position;
+
+public class EnemyGoalTests {
+    @Test
+    @DisplayName("Testing killing spider goal")
+    public void testEnemySpiderGoal() {
+        DungeonManiaController dmc;
+        dmc = new DungeonManiaController();
+        DungeonResponse res = dmc.newGame("d_enemyGoal_simpleSpider.json", "c_complexGoalsTest_andAll");
+
+        assertTrue(getGoals(res).contains(":enemies"));
+        assertFalse(getGoals(res).contains(":treasure"));
+        assertFalse(getGoals(res).contains(":boulders"));
+        assertFalse(getGoals(res).contains(":exit"));
+
+        // kill spider
+        res = dmc.tick(Direction.RIGHT);
+        assertEquals("", getGoals(res));
+    }  
+    
+    @Test
+    @DisplayName("Testing killing mercenary goal")
+    public void testEnemyMercenaryGoal() {
+        DungeonManiaController dmc;
+        dmc = new DungeonManiaController();
+        DungeonResponse res = dmc.newGame("d_enemyGoal_mercenary.json", "c_movementTest_testMovementDown.json");
+
+        assertTrue(getGoals(res).contains(":enemies"));
+        assertFalse(getGoals(res).contains(":treasure"));
+        assertFalse(getGoals(res).contains(":boulders"));
+        assertFalse(getGoals(res).contains(":exit"));
+
+        // kill mercenary
+        res = dmc.tick(Direction.RIGHT);
+        assertEquals("", getGoals(res));
+    }  
+
+    @Test
+    @DisplayName("Testing destroy spawner for enemy goal")
+    public void testEnemySpawnerGoal() {
+        DungeonManiaController dmc;
+        dmc = new DungeonManiaController();
+        DungeonResponse res = dmc.newGame("d_enemyGoal_simpleSpawner.json", "c_movementTest_testMovementDown.json");
+
+        assertTrue(getGoals(res).contains(":enemies"));
+        assertFalse(getGoals(res).contains(":treasure"));
+        assertFalse(getGoals(res).contains(":boulders"));
+        assertFalse(getGoals(res).contains(":exit"));
+
+        // pick up sword
+        res = dmc.tick(Direction.RIGHT);
+        assertEquals(1, getInventory(res, "sword").size());
+        String swordId = getInventory(res, "sword").get(0).getId();
+
+        // destory spawner
+        //res = dmc.interact(swordId);
+        assertEquals("", getGoals(res));
+    }
+
+    @Test
+    @DisplayName("Testing killing multiple spider goal")
+    public void testMultipleEnemySpiderGoal() {
+        DungeonManiaController dmc;
+        dmc = new DungeonManiaController();
+        DungeonResponse res = dmc.newGame("d_enemyGoal_multipleSpider.json", "c_complexGoalsTest_andAll");
+
+        assertTrue(getGoals(res).contains(":enemies"));
+        assertFalse(getGoals(res).contains(":treasure"));
+        assertFalse(getGoals(res).contains(":boulders"));
+        assertFalse(getGoals(res).contains(":exit"));
+
+        // kill spider
+        assertEquals(3, getEntities(res, "spider").size());
+        res = dmc.tick(Direction.RIGHT);
+
+        assertEquals(2, getEntities(res, "spider").size());
+        res = dmc.tick(Direction.RIGHT);
+
+        assertEquals(1, getEntities(res, "spider").size());
+        res = dmc.tick(Direction.RIGHT);
+
+        assertEquals(0, getEntities(res, "spider").size());
+        assertEquals("", getGoals(res));
+    } 
+}
