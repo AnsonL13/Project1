0790189e6a8de1a1abdd545001bceba0693a4165
Trinh <trinh.pham2277@gmail.com>
2022-07-11 17:40:44 +1000

    Change implement goals to class

--- src/main/java/dungeonmania/goals/EnemyGoal.java
+++ src/main/java/dungeonmania/goals/EnemyGoal.java
@@ -1,5 +1,28 @@
 package dungeonmania.goals;
 
-public class EnemyGoal {
+public class EnemyGoal implements Goal {
+
+    @Override
+    public void progessGoal() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean isCompleted() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public String getSimpleName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getString() {
+        return "enemies";
+    }
     
 }
