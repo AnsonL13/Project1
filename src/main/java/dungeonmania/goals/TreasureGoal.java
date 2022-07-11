a67afc2d1138b62406b0bd1bdac0c0cc811f83d9
Trinh <trinh.pham2277@gmail.com>
2022-07-11 11:47:00 +1000

    Change get string function

---
+++ src/main/java/dungeonmania/goals/TreasureGoal.java
@@ -0,0 +1,31 @@
+package dungeonmania.goals;
+
+public class TreasureGoal implements Goal {
+
+    @Override
+    public void progessGoal() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public boolean isCompleted() {
+        // TODO Auto-generated method stub
+        // dungeon.getCollectable entities
+        // dungeon.getInventory
+        return false;
+    }
+
+    @Override
+    public String getSimpleName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public String getString() {
+        // TODO Auto-generated method stub
+        return "treasure";
+    }
+    
+}
