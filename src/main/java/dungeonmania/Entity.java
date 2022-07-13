802b08ab6f0ec55d7230f66a5a7ab0cae710c9b6
Trinh <trinh.pham2277@gmail.com>
2022-07-13 13:50:55 +1000

    correct retuen

--- src/main/java/dungeonmania/Entity.java
+++ src/main/java/dungeonmania/Entity.java
@@ -4,7 +4,7 @@ import dungeonmania.util.Position;
 
 public interface Entity {
     public boolean isInteractable();
-    public String getId();
+    public int getId();
     public String getType();
     public Position getPosition();
 }
