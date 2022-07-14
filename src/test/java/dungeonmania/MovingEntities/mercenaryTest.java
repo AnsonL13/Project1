9bd3addee308de1319c6c8e7b798e8e74a21d21d
Trinh <trinh.pham2277@gmail.com>
2022-07-14 11:18:46 +1000

    Write test for merc moves

--- src/test/java/dungeonmania/MovingEntities/mercenaryTest.java
+++ src/test/java/dungeonmania/MovingEntities/mercenaryTest.java
@@ -6,6 +6,9 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static dungeonmania.TestUtils.getPlayer;
 import static dungeonmania.TestUtils.getEntities;
 import static dungeonmania.TestUtils.getInventory;
@@ -17,6 +20,8 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import dungeonmania.DungeonManiaController;
+import dungeonmania.Entity;
+import dungeonmania.StaticEntities.Wall;
 import dungeonmania.response.models.BattleResponse;
 import dungeonmania.response.models.DungeonResponse;
 import dungeonmania.response.models.EntityResponse;
@@ -44,5 +49,57 @@ public class mercenaryTest {
     }
 
     // White box testing
+    @Test
+    @DisplayName("Test mercenary moves")
+    public void testMercMovesToPlayerStraight() {
+        Position intial = new Position(0, 0);
+        Mercenary mercenary = new Mercenary("1", intial, 1, 1, 1, 1, 1, 1);
+
+        // Expected to move to right
+        mercenary.move(new Position(2, 1), new ArrayList<Entity>());
+        assertEquals(new Position(1, 0), mercenary.getPosition());
+
+        //Move to left
+        mercenary.move(new Position(-2, 1), new ArrayList<Entity>());
+        assertEquals(new Position(0, 0), mercenary.getPosition());
+
+        //Move down
+        mercenary.move(new Position(0, 2), new ArrayList<Entity>());
+        assertEquals(new Position(0, 1), mercenary.getPosition());
+        
+    }
+
+/*  @Test
+    @DisplayName("Test mercenary moves")
+    public void testMercMovesToPlayerDiagonal() {
+        Position intial = new Position(0, 0);
+        Mercenary mercenary = new Mercenary("1", intial, 1, 1, 1, 1, 1, 1);
+
+        // Expected to move to right
+        mercenary.move(new Position(2, 1), new ArrayList<Entity>());
+        assertEquals(new Position(1, 0), mercenary.getPosition());
+
+        //Move to right
+        mercenary.move(new Position(-2, 1), new ArrayList<Entity>());
+        assertEquals(new Position(0, 0), mercenary.getPosition());
+    }*/
+    
+    @Test
+    @DisplayName("Test mercenary moves around wall")
+    public void testMercMovesAroundWall() {
+        List<Entity> stuck = new ArrayList<Entity>();
+        stuck.add(new Wall("0", "Wall", new Position(1, 0), false));
+        stuck.add(new Wall("0", "Wall", new Position(0, 1), false));
+        stuck.add(new Wall("0", "Wall", new Position(0, -1), false));
+
+
+        Position intial = new Position(0, 0);
+        Mercenary mercenary = new Mercenary("1", intial, 1, 1, 1, 1, 1, 1);
+
+        // Expected to move to left
+        mercenary.move(new Position(2, 0), new ArrayList<Entity>());
+        assertEquals(new Position(-1, 0), mercenary.getPosition());
+
+    }
     
 }
