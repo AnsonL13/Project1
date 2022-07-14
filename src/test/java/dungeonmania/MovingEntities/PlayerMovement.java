793616b2db57c8ccd996b80209084731f5e50ffd
unknown <z5362685@ad.unsw.edu.au>
2022-07-14 12:04:36 +1000

    Added player movement test file

---
+++ src/test/java/dungeonmania/MovingEntities/PlayerMovement.java
@@ -0,0 +1,79 @@
+package dungeonmania.MovingEntities;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static dungeonmania.TestUtils.getPlayer;
+import static dungeonmania.TestUtils.getEntities;
+import static dungeonmania.TestUtils.getInventory;
+import static dungeonmania.TestUtils.getGoals;
+import static dungeonmania.TestUtils.countEntityOfType;
+import static dungeonmania.TestUtils.getValueFromConfigFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import dungeonmania.DungeonManiaController;
+import dungeonmania.response.models.BattleResponse;
+import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.response.models.EntityResponse;
+import dungeonmania.response.models.RoundResponse;
+import dungeonmania.util.Direction;
+import dungeonmania.util.Position;
+
+
+public class PlayerMovement {
+    @Test
+    @DisplayName("Test the player can move around")
+    public void testMovementAround() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse initDungonRes = dmc.newGame("d_movementTest_testMovementDown", "c_movementTest_testMovementDown");
+        EntityResponse initPlayer = getPlayer(initDungonRes).get();
+
+        // create the expected result
+        EntityResponse expectedPlayer = new EntityResponse(initPlayer.getId(), initPlayer.getType(), new Position(1, 2), false);
+
+        // move player downward
+        DungeonResponse actualDungonRes = dmc.tick(Direction.DOWN);
+        EntityResponse actualPlayer = getPlayer(actualDungonRes).get();
+
+        // assert after movement
+        assertEquals(expectedPlayer, actualPlayer);
+
+        // create the expected result
+        expectedPlayer = new EntityResponse(initPlayer.getId(), initPlayer.getType(), new Position(1, 1), false);
+
+        // move player downward
+        actualDungonRes = dmc.tick(Direction.UP);
+        actualPlayer = getPlayer(actualDungonRes).get();
+
+        // assert after movement
+        assertEquals(expectedPlayer, actualPlayer);
+
+        // create the expected result
+        expectedPlayer = new EntityResponse(initPlayer.getId(), initPlayer.getType(), new Position(0, 1), false);
+
+        // move player downward
+        actualDungonRes = dmc.tick(Direction.LEFT);
+        actualPlayer = getPlayer(actualDungonRes).get();
+
+        // assert after movement
+        assertEquals(expectedPlayer, actualPlayer);
+
+        // create the expected result
+        expectedPlayer = new EntityResponse(initPlayer.getId(), initPlayer.getType(), new Position(1, 1), false);
+
+        // move player downward
+        actualDungonRes = dmc.tick(Direction.RIGHT);
+        actualPlayer = getPlayer(actualDungonRes).get();
+
+        // assert after movement
+        assertEquals(expectedPlayer, actualPlayer);
+    }
+}
