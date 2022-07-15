6a3b1ec58fccbad0ba0416c30f84c14323d8cb66
Anson Lee <anson.lee1@student.unsw.edu.au>
2022-07-15 16:33:12 +1000

    added tests for battles

--- src/test/java/dungeonmania/BattleTest2.java
+++ src/test/java/dungeonmania/BattleTest2.java
@@ -29,10 +29,10 @@ import dungeonmania.util.Position;
 
 public class BattleTest2 {
 
-    private void assertBattleCalculations(String enemyType, BattleResponse battle, boolean enemyDies, String configFilePath) {
+    private void assertBattleCalculations(String enemyType, BattleResponse battle, boolean enemyDies, String configFilePath, int roundAmount) {
         List<RoundResponse> rounds = battle.getRounds();
         double playerHealth = Double.parseDouble(getValueFromConfigFile("player_health", configFilePath));
-        double enemyHealth = Double.parseDouble(getValueFromConfigFile(enemyType + "_attack", configFilePath));
+        double enemyHealth = Double.parseDouble(getValueFromConfigFile(enemyType + "_health", configFilePath));
         double playerAttack = Double.parseDouble(getValueFromConfigFile("player_attack", configFilePath));
         double enemyAttack = Double.parseDouble(getValueFromConfigFile(enemyType + "_attack", configFilePath));
 
@@ -40,9 +40,10 @@ public class BattleTest2 {
             assertEquals(round.getDeltaCharacterHealth(), -(enemyAttack / 10));
             assertEquals(round.getDeltaEnemyHealth(), -(playerAttack / 5));
             enemyHealth += round.getDeltaEnemyHealth();
-            playerHealth += round.getDeltaCharacterHealth();
+            playerHealth += round.getDeltaCharacterHealth();    
         }
 
+        assertEquals(rounds.size(), roundAmount);
         if (enemyDies) {
             assertTrue(enemyHealth <= 0);
         } else {
@@ -63,8 +64,86 @@ public class BattleTest2 {
         DungeonResponse postBattleResponse = controller.tick(Direction.RIGHT);
         BattleResponse battle = postBattleResponse.getBattles().get(0);
 
-        assertBattleCalculations("zombie", battle, true, "c_battleTest_superInvisiblePotion");
+        assertBattleCalculations("zombie", battle, true, "c_battleTest_superInvisiblePotion", 5);
         assertEquals(1, getEntities(postBattleResponse, "player").size());
         assertEquals(0, getEntities(postBattleResponse, "zombie_toast").size());
+    }
+
+    @Test
+    @DisplayName("Test zombie battle with sword")
+    public void testZombieBattleWithSword() {
+        DungeonManiaController controller = new DungeonManiaController();
+        DungeonResponse initialResponse = controller.newGame("d_battleZombiewithSword", "c_battleTest_superInvisiblePotion");
+        int zombieCount = countEntityOfType(initialResponse, "zombie_toast"); 
+        assertEquals(1, countEntityOfType(initialResponse, "sword"));
+        assertEquals(1, countEntityOfType(initialResponse, "player"));
+        assertEquals(1, zombieCount);
+        
+        // Push the player into the zombie
+        DungeonResponse postBattleResponse = controller.tick(Direction.RIGHT);
+        BattleResponse battle = postBattleResponse.getBattles().get(0);
+
+        List<RoundResponse> rounds = battle.getRounds();
+        double playerHealth = Double.parseDouble(getValueFromConfigFile("player_health", "c_battleTest_superInvisiblePotion"));
+        double enemyHealth = Double.parseDouble(getValueFromConfigFile("zombie" + "_health", "c_battleTest_superInvisiblePotion"));
+        double playerAttack = Double.parseDouble(getValueFromConfigFile("player_attack", "c_battleTest_superInvisiblePotion"));
+        double enemyAttack = Double.parseDouble(getValueFromConfigFile("zombie" + "_attack", "c_battleTest_superInvisiblePotion"));
+
+        double sword = Double.parseDouble(getValueFromConfigFile("sword" + "_attack", "c_battleTest_superInvisiblePotion"));
+
+        for (RoundResponse round : rounds) {
+            assertEquals(round.getDeltaCharacterHealth(), -(enemyAttack / 10));
+            assertEquals(round.getDeltaEnemyHealth(), -((playerAttack + sword) / 5));
+            enemyHealth += round.getDeltaEnemyHealth();
+            playerHealth += round.getDeltaCharacterHealth();    
+        }
+
+        assertTrue(enemyHealth <= 0);
+        assertTrue(playerHealth > 0);
+
+        assertEquals(4, rounds.size());
+        
+        assertEquals(1, getEntities(postBattleResponse, "player").size());
+        assertEquals(0, getEntities(postBattleResponse, "zombie_toast").size());
+        assertEquals(0, getEntities(postBattleResponse, "sword").size());
+    }
+
+    @Test
+    @DisplayName("Test zombie battle with bow")
+    public void testZombieBattleWithBow() {
+        DungeonManiaController controller = new DungeonManiaController();
+        DungeonResponse initialResponse = controller.newGame("d_battleZombieWithBow", "c_battleTest_superInvisiblePotion");
+        int zombieCount = countEntityOfType(initialResponse, "zombie_toast"); 
+        assertEquals(1, countEntityOfType(initialResponse, "bow"));
+        assertEquals(1, countEntityOfType(initialResponse, "player"));
+        assertEquals(1, zombieCount);
+        
+        // Push the player into the zombie
+        DungeonResponse postBattleResponse = controller.tick(Direction.RIGHT);
+        BattleResponse battle = postBattleResponse.getBattles().get(0);
+
+        List<RoundResponse> rounds = battle.getRounds();
+        double playerHealth = Double.parseDouble(getValueFromConfigFile("player_health", "c_battleTest_superInvisiblePotion"));
+        double enemyHealth = Double.parseDouble(getValueFromConfigFile("zombie" + "_health", "c_battleTest_superInvisiblePotion"));
+        double playerAttack = Double.parseDouble(getValueFromConfigFile("player_attack", "c_battleTest_superInvisiblePotion"));
+        double enemyAttack = Double.parseDouble(getValueFromConfigFile("zombie" + "_attack", "c_battleTest_superInvisiblePotion"));
+
+        double bow = 2;
+
+        for (RoundResponse round : rounds) {
+            assertEquals(round.getDeltaCharacterHealth(), -(enemyAttack / 10));
+            assertEquals(round.getDeltaEnemyHealth(), -((playerAttack * bow) / 5));
+            enemyHealth += round.getDeltaEnemyHealth();
+            playerHealth += round.getDeltaCharacterHealth();    
+        }
+
+        assertTrue(enemyHealth <= 0);
+        assertTrue(playerHealth > 0);
+
+        assertEquals(3, rounds.size());
+        
+        assertEquals(1, getEntities(postBattleResponse, "player").size());
+        assertEquals(0, getEntities(postBattleResponse, "zombie_toast").size());
+        assertEquals(0, getEntities(postBattleResponse, "bow").size());
     }
 }
