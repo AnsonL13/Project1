0f844aaa066702836f01986a7f30791873438353
Lucas Wei <weiziyao126126@126.com>
2022-07-17 01:47:25 +1000

    add some static test init

---
+++ src/test/java/dungeonmania/StaticTest.java
@@ -0,0 +1,84 @@
+package dungeonmania;
+
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import static dungeonmania.TestUtils.getPlayer;
+import static dungeonmania.TestUtils.getEntities;
+import static dungeonmania.TestUtils.getInventory;
+import static dungeonmania.TestUtils.getGoals;
+import static dungeonmania.TestUtils.countEntityOfType;
+import static dungeonmania.TestUtils.getValueFromConfigFile;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import dungeonmania.exceptions.InvalidActionException;
+import dungeonmania.response.models.BattleResponse;
+import dungeonmania.response.models.DungeonResponse;
+import dungeonmania.response.models.EntityResponse;
+import dungeonmania.response.models.RoundResponse;
+import dungeonmania.util.Direction;
+import dungeonmania.util.Position;
+
+
+public class StaticTest {
+    @Test
+    @DisplayName("Wall can stop the player")
+    public void test() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse initDungonRes = dmc.newGame("d_wallStopper", "c_movementTest_testMovementDown");
+        EntityResponse initPlayer = getPlayer(initDungonRes).get();
+         // create the expected result
+         EntityResponse expectedPlayer = new EntityResponse(initPlayer.getId(), initPlayer.getType(), new Position(2, 2), false);
+         DungeonResponse actualDungonRes = dmc.tick(Direction.DOWN);
+         EntityResponse actualPlayer = getPlayer(actualDungonRes).get();
+         // assert after movement
+        assertEquals(expectedPlayer, actualPlayer);
+    }
+
+    @Test
+    @DisplayName("player can go through the exit")
+    public void ExitTest() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse initDungonRes = dmc.newGame("d_wallStopper", "c_movementTest_testMovementDown");
+        EntityResponse initPlayer = getPlayer(initDungonRes).get();
+         // create the expected result
+         EntityResponse expectedPlayer = new EntityResponse(initPlayer.getId(), initPlayer.getType(), new Position(1, 2), false);
+         DungeonResponse actualDungonRes = dmc.tick(Direction.LEFT);
+         EntityResponse actualPlayer = getPlayer(actualDungonRes).get();
+         // assert after movement
+        assertEquals(expectedPlayer, actualPlayer);
+    }
+
+    @Test
+    @DisplayName("player can push the boulder")
+    public void BoulderTest() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse initDungonRes = dmc.newGame("d_wallStopper", "c_movementTest_testMovementDown");
+        EntityResponse initPlayer = getPlayer(initDungonRes).get();
+         // create the expected result
+         EntityResponse expectedPlayer = new EntityResponse(initPlayer.getId(), initPlayer.getType(), new Position(3, 2), false);
+         DungeonResponse actualDungonRes = dmc.tick(Direction.RIGHT);
+         EntityResponse actualPlayer = getPlayer(actualDungonRes).get();
+         // assert after movement
+        assertEquals(expectedPlayer, actualPlayer);
+    }
+
+   
+
+    
+    
+    
+
+        
+
+    
+}
