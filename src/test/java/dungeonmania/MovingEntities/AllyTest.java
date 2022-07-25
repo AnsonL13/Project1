beb5852cabf12c202bdc6bb062a0e989bc68e5b5
Trinh <trinh.pham2277@gmail.com>
2022-07-25 18:55:09 +1000

    adding ally test

--- src/test/java/dungeonmania/MovingEntities/AllyTest.java
+++ src/test/java/dungeonmania/MovingEntities/AllyTest.java
@@ -2,6 +2,8 @@ package dungeonmania.MovingEntities;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import static dungeonmania.TestUtils.getEntities;
 import static dungeonmania.TestUtils.getInventory;
@@ -12,8 +14,11 @@ import static dungeonmania.TestUtils.countEntityOfType;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import dungeonmania.Battle;
 import dungeonmania.DungeonManiaController;
 import dungeonmania.Entity;
+import dungeonmania.Player;
+import dungeonmania.MovingEntities.Mercenary;
 import dungeonmania.response.models.DungeonResponse;
 import dungeonmania.response.models.EntityResponse;
 import dungeonmania.util.Direction;
@@ -26,7 +31,6 @@ public class AllyTest {
     public void testMercenaryAlliedMovement() {
         DungeonManiaController dmc = new DungeonManiaController();
         DungeonResponse initDungonRes = dmc.newGame("d_bribeMercenary", "c_battleTests_basicMercenaryMercenaryDies");
-
 
         initDungonRes = dmc.tick(Direction.RIGHT);
         initDungonRes = dmc.tick(Direction.RIGHT);
@@ -89,4 +93,23 @@ public class AllyTest {
         assertEquals(expectedAlly, merc.getPosition());  
 
     }
+
+    // white box testing with allied merc
+
+    @Test
+    @DisplayName("Test simple zombie battle with ally")
+    public void testAllyZombieDie() {
+        // Create zombie and player
+        Position intial = new Position(0, 0);
+        ZombieToast zombie = new ZombieToast("0", 5, 2, intial);
+        Player player = new Player("1", "player", intial.translateBy(Direction.DOWN), false, 1, 2);
+        Mercenary merc = new Mercenary("1", new Position(0, 2),4,0,1,1,1,1);
+        player.addAlly(merc);
+        Battle result = Battle.battleCalculate(player, zombie);
+
+        // check results
+        //assertTrue(result.isPlayerWon());
+        //assertFalse(result.isEnemyWon());
+        assertEquals(2, result.getRounds().size());
+    } 
 }
