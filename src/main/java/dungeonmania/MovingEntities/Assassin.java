89f536b5b2f8835c84be0df437d1aaabedba1d43
Trinh <trinh.pham2277@gmail.com>
2022-07-25 18:31:17 +1000

    changeing style

--- src/main/java/dungeonmania/MovingEntities/Assassin.java
+++ src/main/java/dungeonmania/MovingEntities/Assassin.java
@@ -3,9 +3,9 @@ package dungeonmania.MovingEntities;
 import java.util.List;
 import java.util.Random;
 
-import dungeonmania.util.Position;
 import dungeonmania.Dungeon;
 import dungeonmania.Entity;
+import dungeonmania.util.Position;
 
 public class Assassin extends Mercenary {
     private int reconRadius;
@@ -30,8 +30,17 @@ public class Assassin extends Mercenary {
 
     }
     // TODO move for invisible potion
-    //@Override
-    //public void move(Position playerPos, List<Entity> entities) {}
+    @Override
+    public void move(Position playerPos, List<Entity> entities) {
+        boolean changedPotionStat = false;
+        if (super.isInvisible() && canSeePlayer(playerPos)) {
+            super.setPotionStatus(false, super.isInvincible());
+            changedPotionStat = true;
+        }
+
+        super.move(playerPos, entities);
+        if (changedPotionStat) super.setPotionStatus(true, super.isInvincible());
+    }
     
     @Override 
     public void interact(Dungeon dungeon) {
@@ -39,7 +48,24 @@ public class Assassin extends Mercenary {
 
         Random rand = new Random(); //instance of random class
         Double chance = rand.nextDouble(); // check if 0 to 1
-        if (chance > failRate) return; // if success
-        super.setAllied(false);
+        if (chance <= failRate) { // fail
+            super.setAllied(false);
+        }
+    }
+
+
+    private boolean canSeePlayer(Position player) {
+        int xTopBoundary = super.getPosition().getX() + reconRadius;
+        int xBottomBoundary = super.getPosition().getX() - reconRadius;
+        int yTopBoundary = super.getPosition().getY() + reconRadius;
+        int yBottomBoundary = super.getPosition().getY() - reconRadius;
+
+        // Check if player is radius of invalid invisible
+        if (player.getX() >= xBottomBoundary && player.getX() <= xTopBoundary &&
+            player.getY() >= yBottomBoundary && player.getY() <= yTopBoundary) {
+                return true;
+        }
+
+        return false;
     }
 }
