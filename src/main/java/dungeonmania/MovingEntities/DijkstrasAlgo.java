6cb83da0c9dcdc1774fb479c958ad2d3ffe7f060
Trinh <trinh.pham2277@gmail.com>
2022-07-26 19:58:00 +1000

    refactor code

--- src/main/java/dungeonmania/MovingEntities/DijkstrasAlgo.java
+++ src/main/java/dungeonmania/MovingEntities/DijkstrasAlgo.java
@@ -6,7 +6,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.PriorityQueue;
 import java.util.Queue;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import dungeonmania.Entity;
@@ -19,7 +18,7 @@ import dungeonmania.util.Position;
 
 
 public class DijkstrasAlgo {
-    private GraphNode adj_list[][] = new GraphNode[40][40];
+    private GraphNode graph[][] = new GraphNode[40][40];
     private Map<GraphNode, GraphNode> previous = new HashMap<>();
     private Map<String, List<Portal>> portals = new HashMap<String, List<Portal>>();
     private Position entity;
@@ -28,10 +27,14 @@ public class DijkstrasAlgo {
         this.entity = entity.translateBy(20, 20);
     }
 
+    /**
+     * Function to generate graph with each position as a vertex
+     * @param entities
+     */
     public void generateMap (List<Entity> entities) {
-        for (int i = 0; i < 40; i++) {
+        for (int i = 0; i < 40; i++) { // add all positions
             for (int j = 0; j < 40; j++) {
-                adj_list[i][j] = new GraphNode(1, new Position(i, j));
+                graph[i][j] = new GraphNode(1, new Position(i, j));
             }
         }
         for (Entity entity: entities) {
@@ -39,14 +42,14 @@ public class DijkstrasAlgo {
                 Position block = entity.getPosition().translateBy(20,20);
                 int x = block.getX();
                 int y = block.getY();
-                adj_list[x][y].setBlocked();
+                graph[x][y].setBlocked();
             } else if (entity instanceof SwampTile) {
                 Position block = entity.getPosition().translateBy(20,20);
                 int x = block.getX();
                 int y = block.getY();
                 SwampTile swampTile = (SwampTile) entity;
-                adj_list[x][y].setCost(swampTile.getMovementFactor());
-            } else if (entity instanceof Portal) {
+                graph[x][y].setCost(swampTile.getMovementFactor());
+            } /*else if (entity instanceof Portal) {
                 // TODO
                 Portal currPortal = (Portal) entity;
                 String colour = currPortal.getColour();
@@ -59,38 +62,17 @@ public class DijkstrasAlgo {
                     portals.put(colour, match);
                 }
 
-            }
+            }*/
         }
-        for (Map.Entry<String, List<Portal>> match : portals.entrySet()) {
-
-        }
-    }
-
-    public void printMap() {
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
-                Position block = adj_list[i][j].getPos();
-                int x = block.getX();
-                int y = block.getY();
-                if (!adj_list[i][j].isBlocked()) {
-                    System.out.printf("[%d,%d]", x,y);
-                } else {
-                    System.out.printf("[X,X]", x,y);
-
-                }
-                
-            }
-            System.out.println("\n");
-        }
+        //for (Map.Entry<String, List<Portal>> match : portals.entrySet()) {}
     }
 
     public void printPath(Position player) {
         player = player.translateBy(20,20);
 
-        GraphNode node = adj_list[player.getX()][player.getY()];
+        GraphNode node = graph[player.getX()][player.getY()];
         Position pos = node.getPos();
 
-        System.out.println("End");
         if (previous.get(node) == null) return;
         while (!pos.equals(entity)) {
             System.out.printf("[%d,%d]\n", pos.translateBy(-20,-20).getX(), pos.translateBy(-20,-20).getY());
@@ -102,41 +84,21 @@ public class DijkstrasAlgo {
     }
 
     public Position getNextPos(Position player) {
-        player = player.translateBy(20,20);
-        GraphNode node = adj_list[player.getX()][player.getY()];
+        player = player.translateBy(20,20); // change position to match map
+        GraphNode node = graph[player.getX()][player.getY()];
         Position pos = node.getPos();
         Position prev = null;
 
         // entity is blocked
         if (previous.get(node) == null) return entity.translateBy(-20,-20);
 
-        while (!pos.equals(entity)) {
+        while (!pos.equals(entity)) { // go through shortest path
             prev = pos;
             node = previous.get(node);
             pos = node.getPos();
         }
-        return prev.translateBy(-20,-20); // TODO
+        return prev.translateBy(-20,-20); // return next position
     }
-
-    
-    /*function Dijkstras(grid, source):
-    let dist be a Map<Position, Double>
-    let prev be a Map<Position, Position>
-
-    for each Position p in grid:
-        dist[p] := infinity
-        previous[p] := null
-    dist[source] := 0
-
-    let queue be a Queue<Position> of every position in grid
-    while queue is not empty:
-        u := next node in queue with the smallest dist
-        for each cardinal neighbour v of u:
-            if dist[u] + cost(u, v) < dist[v]:
-                dist[v] := dist[u] + cost(u, v)
-                previous[v] := u
-    return previous
- */
 
     public void DijstrasPosition () {
         Map<GraphNode, Double> dist = new HashMap<>();
@@ -147,30 +109,28 @@ public class DijkstrasAlgo {
 
         double inf = Double.POSITIVE_INFINITY;
         
+        // add all valid nodes
         for (int i = 0; i < 40; i++) {
             for (int j = 0; j < 40; j++) {
-                if (i == startX && j == startY) { // check if need to check for blocke nodes
-                    dist.put(adj_list[i][j], 0.0);
-                    previous.put(adj_list[i][j], null);
-                } else if (! adj_list[i][j].isBlocked()) {
-                    dist.put(adj_list[i][j], inf);
-                    previous.put(adj_list[i][j], null);
+                if (i == startX && j == startY) { 
+                    dist.put(graph[i][j], 0.0);
+                    previous.put(graph[i][j], null);
+                } else if (! graph[i][j].isBlocked()) {
+                    dist.put(graph[i][j], inf);
+                    previous.put(graph[i][j], null);
                 }
             }
         }
-       // dist.replace(new Node(1, new Position(1, 2)), 0.0); //replace with entity
 
-        //queue.add(new Node(1, new Position(1, 2)));
         //System.out.printf("[%d,%d]\n", startX, startY);
 
-        queue.add(adj_list[startX][startY]);
+        queue.add(graph[startX][startY]);
         GraphNode pos = null;
 
         int count = 0;
         while(!queue.isEmpty()) {
             pos = queue.poll();
             if (dist.get(pos) == Integer.MAX_VALUE) {
-                System.out.println("broken");
 				break;
 			}
             count++;
@@ -196,24 +156,19 @@ public class DijkstrasAlgo {
 
                 //count+= connect.getCost();
 
-
             }
         }
         //System.out.println(count);
     }
 
-    public Position nextPos() {
-        return null;
-    }
-
-    public List<GraphNode> getCard(GraphNode node) {
+    private List<GraphNode> getCard(GraphNode node) {
         List<GraphNode> cardList = new ArrayList<GraphNode>();
         int x = node.getPos().getX();
         int y = node.getPos().getY();
-        if (x + 1 < 40) cardList.add(adj_list[x + 1][y]);
-        if (x - 1 >= 0) cardList.add(adj_list[x - 1][y]);
-        if (y + 1 < 40) cardList.add(adj_list[x][y + 1]);
-        if (y - 1 >= 0) cardList.add(adj_list[x][y - 1]);
+        if (x + 1 < 40) cardList.add(graph[x + 1][y]);
+        if (x - 1 >= 0) cardList.add(graph[x - 1][y]);
+        if (y + 1 < 40) cardList.add(graph[x][y + 1]);
+        if (y - 1 >= 0) cardList.add(graph[x][y - 1]);
     
         return cardList.stream().filter(e -> ! e.isBlocked()).collect(Collectors.toList());
     }
@@ -229,7 +184,6 @@ public class DijkstrasAlgo {
         entity.add(new SwampTile("1", "swamp_time", new Position(2, 1), false, 5));
 
         check.generateMap(entity);
-        check.printMap();
 
         check.DijstrasPosition();
         Position play = new Position(1,1);
