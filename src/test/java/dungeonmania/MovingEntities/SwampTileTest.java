26df51e61f2828ea78dd6879204bf68b779421dc
Trinh <trinh.pham2277@gmail.com>
2022-07-26 19:15:36 +1000

    get rid of unused import

--- src/test/java/dungeonmania/MovingEntities/SwampTileTest.java
+++ src/test/java/dungeonmania/MovingEntities/SwampTileTest.java
@@ -1,7 +1,6 @@
 package dungeonmania.MovingEntities;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
 
 import java.util.ArrayList;
 import java.util.List;
