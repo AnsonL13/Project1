bb288ac002d9696644b3c759f26b3ba5fe1857e1
Trinh <trinh.pham2277@gmail.com>
2022-07-29 22:12:03 +1000

    adding anothe test

--- src/test/java/dungeonmania/MovingEntities/TimeTurnerTest.java
+++ src/test/java/dungeonmania/MovingEntities/TimeTurnerTest.java
@@ -2,7 +2,11 @@ package dungeonmania.MovingEntities;
 
 import static dungeonmania.TestUtils.getInventory;
 import static dungeonmania.TestUtils.getEntities;
+import static dungeonmania.TestUtils.countEntityOfType;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -14,19 +18,39 @@ import dungeonmania.util.Direction;
 
 public class TimeTurnerTest {
     @Test
-    @DisplayName("Test zombie is spawned")
-    public void testZombieSpawned() {
+    @DisplayName("Test time turner collected")
+    public void testTimeTurnerCollected() {
         DungeonManiaController dmc = new DungeonManiaController();
-        DungeonResponse initDungonRes = dmc.newGame("d_timeTurner_simpleSpawn", "c_zombieTest_simpleSpawn");
+        DungeonResponse initDungonRes = dmc.newGame("TimeTurner/d_timeTurner_simpleSpawn", "c_zombieTest_simpleSpawn");
 
         // check timerTurner created
-        assertEquals(1, getEntities(initDungonRes, "time_turner"));
+        assertEquals(1, countEntityOfType(initDungonRes, "time_turner"));
 
         // pick up timeTurner
-        dmc.tick(Direction.RIGHT);
+        initDungonRes = dmc.tick(Direction.RIGHT);
 
         // check inventory
-        assertEquals(1, getInventory(initDungonRes, "time_turner"));
+        assertEquals(1, getInventory(initDungonRes, "time_turner").size());
+
+    }
+
+    @Test
+    @DisplayName("Test invalid time travel")
+    public void testExceptionInvalidTime() {
+        DungeonManiaController dmc = new DungeonManiaController();
+        DungeonResponse initDungonRes = dmc.newGame("TimeTurner/d_timeTurner_simpleSpawn", "c_zombieTest_simpleSpawn");
+
+        assertThrows(IllegalArgumentException.class, () -> dmc.rewind(1));
+        
+        // check timerTurner created
+        assertEquals(1, countEntityOfType(initDungonRes, "time_turner"));
+
+        // pick up timeTurner
+        initDungonRes = dmc.tick(Direction.RIGHT);
+        assertDoesNotThrow(() -> dmc.rewind(1));
+
+        // check inventory
+        assertEquals(1, getInventory(initDungonRes, "time_turner").size());
 
     }
     
