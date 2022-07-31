db0dbc5765f0eb1162db28ae421cf3cafc9be908
Anson Lee <anson.lee1@student.unsw.edu.au>
2022-07-31 11:16:00 +1000

    Added extra dungeon generation test

--- src/test/java/dungeonmania/DungeonGenerationTest.java
+++ src/test/java/dungeonmania/DungeonGenerationTest.java
@@ -3,6 +3,9 @@ package dungeonmania;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import java.util.List;
+
 import static dungeonmania.TestUtils.getEntities;
 
 import org.junit.jupiter.api.DisplayName;
@@ -21,5 +24,23 @@ public class DungeonGenerationTest {
 
         assertDoesNotThrow(() -> dmc.generateDungeon(-50, -50, 0, 0, "simple")); 
         assertThrows(IllegalArgumentException.class, () -> dmc.generateDungeon(-50, -50, 50, 50, "shflsfje")); 
+    }
+
+    @Test
+    @DisplayName("Test start, and exit is correct position")
+    public void testMazePositions() {
+        DungeonManiaController dmc = new DungeonManiaController();
+
+        DungeonResponse res = dmc.generateDungeon(-29, -29, 29, 29, "bomb_radius_2"); 
+
+        EntityResponse exit = getEntities(res,"exit").get(0);
+
+        Position expectedExitPosition = new Position(29, 29);
+        assertEquals(expectedExitPosition, exit.getPosition());
+
+        EntityResponse player = getEntities(res, "player").get(0);
+
+        Position expectedPlayerPosition = new Position(-29, -29);
+        assertEquals(expectedPlayerPosition, player.getPosition());
     }
 }
